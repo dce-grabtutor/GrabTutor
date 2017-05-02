d010bff068153e35804fc48c59948c4145d71ca9
dce-grabtutor <dce.grabtutor@gmail.com>
2017-05-02 13:43:32 +0800

    Origin

--- app/src/main/java/com/dce/grabtutor/StudentTutorSearchSettingsActivity.java
+++ app/src/main/java/com/dce/grabtutor/StudentTutorSearchSettingsActivity.java
@@ -1,13 +1,13 @@
 package com.dce.grabtutor;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.EditText;
 
 public class StudentTutorSearchSettingsActivity extends AppCompatActivity {
+
+    EditText etSearchRangeMax;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +15,9 @@ public class StudentTutorSearchSettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_student_tutor_search_settings);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        setTitle("Search Settings");
+
+        etSearchRangeMax = (EditText) findViewById(R.id.etSearchRangeMax);
     }
 
 }
