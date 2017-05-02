288e9b5b1db2aec81cae260ad92f67fa640e99ce
dce-grabtutor <dce.grabtutor@gmail.com>
2017-05-02 16:30:22 +0800

    Origin

--- app/src/main/java/com/dce/grabtutor/StudentTutorSearchScheduleManagementActivity.java
+++ app/src/main/java/com/dce/grabtutor/StudentTutorSearchScheduleManagementActivity.java
@@ -1,18 +1,15 @@
 package com.dce.grabtutor;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
-public class StudentTutorSearchScheduleActivity extends AppCompatActivity {
+public class StudentTutorSearchScheduleManagementActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_student_tutor_search_schedule);
+        setContentView(R.layout.activity_student_tutor_search_schedule_management);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
