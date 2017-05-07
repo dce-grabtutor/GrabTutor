df31ec3a05eb76c3918453900132228f71a60150
dce-grabtutor <dce.grabtutor@gmail.com>
2017-05-07 22:33:10 +0800

    Origin

--- app/src/main/java/com/dce/grabtutor/Model/URI.java
+++ app/src/main/java/com/dce/grabtutor/Model/URI.java
@@ -22,9 +22,9 @@ public class URI {
     public static final String SCHEDULE_ADD = HOST + "schedule_add.php";
     public static final String SCHEDULE_REMOVE = HOST + "schedule_remove.php";
     public static final String SCHEDULE_LOAD = HOST + "schedule_load.php";
-    public static final String SCHEDULE_LOAD_DAY = HOST + "schedule_load_day.php";
+//    public static final String SCHEDULE_LOAD_DAY = HOST + "schedule_load_day.php";
 
-    public static final String APTITUDE_START = HOST + "aptitude_start.php";
+    //    public static final String APTITUDE_START = HOST + "aptitude_start.php";
     public static final String APTITUDE_EXAM = HOST + "aptitude_exam.php";
     public static final String APTITUDE_STATUS_CHECK = HOST + "aptitude_status_check.php";
 
